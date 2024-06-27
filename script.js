f3df006e4eba76e3e38532ce7c8bd831e374d905
sharan-a11y <nishadhiman903@gmail.com>
2024-06-27 23:41:20 +0530

    Add files via upload
    this is all about me

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// You can add JavaScript functionality here, such as dynamic content loading or animations
+// Example:
+document.querySelector('.project').addEventListener('click', function() {
+    alert('Project clicked!');
+}); 
